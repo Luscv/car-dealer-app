c049b2e316491da2872d797eb76938dad6b88018
Luscv <lucbarrospc@gmail.com>
2024-10-01 23:58:06 -0300

    generateStaticParams done

--- src/app/result/[makeId]/[year]/page.jsx
+++ src/app/result/[makeId]/[year]/page.jsx
@@ -1,12 +1,17 @@
 
-
-// import { useRouter } from "next/router"
-// import { Suspense, useEffect, useState } from "react"
+import { Suspense } from "react"
+async function fetchData(makeId, year){
+    let data = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`)
+    let response = await data.json()
+    return response.Results
+}
 
 export async function generateStaticParams(){
     const data = await fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
     let response = await data.json()
     let makes = response.Results
+
+    
 
     return makes.map((make) => ({
         makeId: make.MakeId.toString(),
@@ -16,49 +21,24 @@ export async function generateStaticParams(){
 }
 
 
-export default function ResultPage({params}){
-    const router = useRouter()
+
+export default async function ResultPage({params}){
     const {makeId, year} = params
-    // const [models, setModels] = useState([])
-    // const [error, setError] = useState(null)
-
-    // async function fetchData(){
-    //     let data = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`)
-    //     .catch(err => setError(err))
-    //     let response = await data.json()
-    //     console.log(response)
-    //     // setModels(response.Results)
-    // }
-
-    // useEffect(() => {
-    //     console.log(makeId, year)
-    //     if(makeId && year){
-    //         fetchData()
-    //     }
-    // }, [makeId, year])
-
-    // if(error) {
-    //     return(
-    //         <div>Error Loading data: {error.message}</div>
-    //     )
-    // }
-
+    const models = await fetchData(makeId, year)
     return (
-        <div className="bg-zinc-950 h-screen">
-            {/* <h1>Vehicle Models for {year}</h1>
-
+        <div className="bg-zinc-950 h-screen flex justify-center items-center">
             <Suspense fallback={<div>Loading...</div>}>
                 <div>
                     {
                         models.length > 0 ? models.map((model) => (
                             <div key={model.Model_ID}>
-                                <p>{model.Model_Name}</p>
+                                <p>{model.Make_Name} - {model.Model_Name}</p>
                             </div>
                         )) : <div> No models available for this filter</div>
                     }
                 </div>
-            </Suspense> */}
-            testeeeee
+            </Suspense>
+            
         </div>
     )
 }
